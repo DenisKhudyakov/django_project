6e9c81b7ec7e7eea4a32fd266c3b5af64b0af808
Denis Khudyakov <denis.hudyakov27@gmail.com>
2024-03-07 21:32:30 +0500

    задание 4 выполнено

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,10 +2,7 @@ from django.urls import path
 
 from catalog.views import contacts, home
 
-
-app_name = 'contacts'
-
 urlpatterns = [
-    path('', contacts, name='contacts'),
-    path('', home, name='home')
+    path('', home),
+    path('contacts/', contacts)
 ]
