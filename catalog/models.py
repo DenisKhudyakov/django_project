d79165b1c0bccb9f4d40be7609684a0adf9d3dae
Denis Khudyakov <denis.hudyakov27@gmail.com>
2024-03-13 20:55:53 +0500

    task2 done

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+
+    def __str__(self):
+        return f'{self.__class__.__name__}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+
+class Category(models.Model):
+
+    def __str__(self):
+        return f'{self.__class__.__name__}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
