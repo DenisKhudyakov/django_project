e9106f20730e841a86b24facb7a858362b07bf0e
Denis Khudyakov <denis.hudyakov27@gmail.com>
2024-03-15 20:51:48 +0500

    task3 done

--- catalog/models.py
+++ catalog/models.py
@@ -3,23 +3,6 @@ from django.db import models
 NULL_PARAM = {'null': True, 'blank': True}
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name='имя')
-    description = models.TextField(max_length=150, verbose_name='описание товара')
-    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULL_PARAM)
-    category = models.CharField(max_length=50, verbose_name='категория')
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена в руб')
-    date_of_creation = models.DateField(verbose_name='дата создания')
-    date_of_the_last_change = models.DateField(verbose_name='дата последнего изменения')
-
-    def __str__(self):
-        return f'{self.name} - {self.price}'
-
-    class Meta:
-        verbose_name = 'продукт'
-        verbose_name_plural = 'продукты'
-
-
 class Category(models.Model):
     name = models.CharField(max_length=50, verbose_name='наименование')
     description = models.TextField(max_length=150, verbose_name='описание')
@@ -30,3 +13,23 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='имя')
+    description = models.TextField(max_length=150, verbose_name='описание товара')
+    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULL_PARAM)
+    category = models.ForeignKey(Category, verbose_name='категория', on_delete=models.CASCADE, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена в руб')
+    created_at = models.DateField(verbose_name='дата создания')
+    updated_at = models.DateField(verbose_name='дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name} - {self.price}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+
+
