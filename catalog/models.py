a23e1c59a496f31f890a4ebe4a44518e1d357328
Denis Khudyakov <denis.hudyakov27@gmail.com>
2024-03-15 19:57:34 +0500

    create atribute class products

--- catalog/models.py
+++ catalog/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
+NULL_PARAM = {'null': True, 'blank': True}
+
 
 class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='имя')
+    description = models.TextField(max_length=150, verbose_name='описание товара')
+    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULL_PARAM)
+    category = models.CharField(max_length=50, verbose_name='категория')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена в руб')
+    date_of_creation = models.DateField(verbose_name='дата создания')
+    date_of_the_last_change = models.DateField(verbose_name='дата последнего изменения')
 
     def __str__(self):
         return f'{self.__class__.__name__}'
@@ -19,4 +28,3 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
-
