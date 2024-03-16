405a4b09c937f4b8c5c637c27b80568c743209c0
Denis Khudyakov <denis.hudyakov27@gmail.com>
2024-03-16 13:19:24 +0500

    remove manufactured_at

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,11 +5,13 @@ from catalog.models import Product, Category
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'category')
-    list_filter = 'category'
+    list_display = ('pk', 'name', 'price', 'category')
+    list_filter = ['category',]
     search_fields = ['name', 'description']
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
+    list_display = ('pk', 'name', 'description')
+    list_filter = ['name', ]
+    search_fields = ['name', 'description']
