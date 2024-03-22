bc387c7a7afc77eb19d3d23f030dc782789a4a88
Denis Khudyakov <denis.hudyakov27@gmail.com>
2024-03-22 21:49:39 +0500

    create tas1,2,3

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from catalog.views import contacts, basket
+from catalog.views import contacts, basket, card
 from catalog.apps import CatalogConfig
 
 app_name = CatalogConfig.name
@@ -8,4 +8,5 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', basket, name='basket'),
     path('contacts/', contacts, name='contacts'),
+    path('card/<int:pk>/', card, name='card'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def contacts(request):
-    return render(request,'catalog/contacts.html')
+    return render(request, 'catalog/contacts.html')
 
 
 def basket(request):
-    return render(request, 'catalog/basket.html')
+    context = {
+        'products': Product.objects.all()
+    }
+    return render(request, 'catalog/basket.html', context=context)
 
+
+def card(request, pk):
+    product = {
+        'products': Product.objects.get(pk=pk)
+    }
+    return render(request, 'catalog/product_card.html', context=product)
