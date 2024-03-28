02976d22970896d0ae39a6f935b7fc73f91e64cf
Denis Khudyakov <denis.hudyakov27@gmail.com>
2024-03-28 21:05:12 +0500

    create two controllers

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import BlogCreateView
+from blog.views import BlogCreateView, BlogListView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('create/', BlogCreateView, name='product'),
+    path('create/', BlogCreateView.as_view(), name='product'),
+    path('view/', BlogListView.as_view(), name='list'),
     path('view/<int:pk>/', ..., name='view'),
     path('edit/<int:pk>/', ..., name='edit'),
     path('delete/<int:pk>/', ..., name='delete'),
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
 from blog.models import Blog
 
 
 class BlogCreateView(CreateView):
+    model = Blog
+
+
+class BlogListView(ListView):
     model = Blog
