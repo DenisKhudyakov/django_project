3ad7109a9c8b9d54ec4563e5ad98f7ee3df14a2c
Denis Khudyakov <denis.hudyakov27@gmail.com>
2024-03-29 20:41:35 +0500

    task2 done

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import CreateView, ListView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 
 from blog.models import Blog
 
@@ -7,7 +7,28 @@ from blog.models import Blog
 class BlogCreateView(CreateView):
     model = Blog
     fields = ('title', 'content', 'preview')
+    success_url = reverse_lazy('blog:list')
 
 
 class BlogListView(ListView):
     model = Blog
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(publication=True)
+        return queryset
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ('title', 'content', 'preview')
+    success_url = reverse_lazy('blog:list')
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy('blog:list')
