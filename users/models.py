cbdce812e7662708ff112ba9e659300cb896fe5e
Denis Khudyakov <denis.hudyakov27@gmail.com>
2024-04-20 12:03:21 +0500

    make registration user

--- users/models.py
+++ users/models.py
@@ -1,13 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from blog.models import NULL_PARAM
+
 
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='email')
-    avatar = models.ImageField(upload_to='media/avatars/', verbose_name='avatar', null=True, blank=True)
-    number_tel = models.CharField(max_length=11, unique=True, verbose_name='telephone_number', help_text="Введите номер телефона")
-    country = models.CharField(max_length=12, verbose_name='country', help_text="Введите страну")
+
+    avatar = models.ImageField(upload_to='media/avatars/',**NULL_PARAM, verbose_name='avatar')
+    number_tel = models.CharField(max_length=11, unique=True, **NULL_PARAM, verbose_name='telephone_number', help_text="Введите номер телефона")
+    country = models.CharField(max_length=12, **NULL_PARAM, verbose_name='country', help_text="Введите страну")
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
